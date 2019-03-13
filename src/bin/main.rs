e8efb71b935110e853ee1bfd2a55ba85acd56648
lesnyrumcajs <lesny.rumcajs@gmail.com>
2019-03-13 21:05:35 +0100

    initial

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,4 @@
-use clap::{App, Arg, SubCommand, crate_version, crate_authors, crate_name, crate_description};
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg, SubCommand};
 use ryptor::crypt::Encryptor;
 
 fn main() {
@@ -7,29 +7,41 @@ fn main() {
         .author(crate_authors!())
         .about(crate_description!())
         .subcommand(
-            SubCommand::with_name("encrypt").about("Encrypts data").arg(
-                Arg::with_name("path")
-                    .index(1)
-                    .required(true)
-            ),
+            SubCommand::with_name("encrypt")
+                .about("Encrypts data")
+                .arg(Arg::with_name("path").index(1).required(true)),
         )
         .subcommand(
-            SubCommand::with_name("decrypt").about("Decrypts data").arg(
-                Arg::with_name("path")
-                    .index(1)
-                    .required(true)
-            ),
+            SubCommand::with_name("decrypt")
+                .about("Decrypts data")
+                .arg(Arg::with_name("path").index(1).required(true)),
         )
         .get_matches();
 
     match matches.subcommand_name() {
         Some("encrypt") => {
-            println!("Encrypting: {}", matches.subcommand_matches("encrypt").unwrap().value_of("path").unwrap());
-            let result = Encryptor::new();
-        },
-        Some("decrypt") => {
-            println!("Decrypting: {}", matches.subcommand_matches("decrypt").unwrap().value_of("path").unwrap())
-        },
-        _ => println!("Unknown command")
+            println!(
+                "Encrypting: {}",
+                matches
+                    .subcommand_matches("encrypt")
+                    .unwrap()
+                    .value_of("path")
+                    .unwrap()
+            );
+            let encryptor = Encryptor::new();
+            match encryptor.save_key() {
+                Ok(result) => println!("Saved the key to: {}", result),
+                Err(e) => println!("Error saving file: {}", e),
+            }
+        }
+        Some("decrypt") => println!(
+            "Decrypting: {}",
+            matches
+                .subcommand_matches("decrypt")
+                .unwrap()
+                .value_of("path")
+                .unwrap()
+        ),
+        _ => println!("Unknown command"),
     }
 }
--- src/crypt.rs
+++ src/crypt.rs
@@ -1,14 +1,19 @@
 use rand::Rng;
+use sha2::{Digest, Sha256};
+use std::fs;
 
 const AES_BLOCK_SIZE: usize = 16;
 
 pub struct Encryptor {
-    key: [u8; AES_BLOCK_SIZE]
+    key: [u8; AES_BLOCK_SIZE],
 }
 
 impl Encryptor {
+    #[allow(clippy::new_without_default)]
     pub fn new() -> Encryptor {
-        Encryptor { key: Encryptor::generate_key()}
+        Encryptor {
+            key: Encryptor::generate_key(),
+        }
     }
 
     fn generate_key() -> [u8; AES_BLOCK_SIZE] {
@@ -20,7 +25,14 @@ impl Encryptor {
     }
 
     /// Saves the current key to file
-    pub fn save_key() -> Result<(), std::io::Error> {
-        unimplemented!()
+    pub fn save_key(&self) -> Result<String, std::io::Error> {
+        let mut hasher = Sha256::new();
+        hasher.input(&self.key);
+        let hash = hex::encode(hasher.result())[0..=6].to_owned();
+
+        let encoded_key = base64::encode(&self.key);
+        fs::write(&hash, encoded_key)?;
+
+        Ok(hash)
     }
 }
