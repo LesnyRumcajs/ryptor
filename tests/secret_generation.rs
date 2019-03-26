95aa827e1217aacf4254ee72e78c0eab1b393a37
lesnyrumcajs <lesny.rumcajs@gmail.com>
2019-03-26 22:43:36 +0100

    typed paths

--- tests/secret_generation.rs
+++ tests/secret_generation.rs
@@ -1,12 +1,12 @@
 use ryptor::crypt;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 #[test]
 fn should_generate_random_key() {
-    let key_file = "should_generate_random_key";
+    let key_file = PathBuf::from("should_generate_random_key");
 
     let encryptor = crypt::Encryptor::new();
-    encryptor.save_key(key_file).unwrap();
+    encryptor.save_key(key_file.as_path()).unwrap();
 
     assert!(Path::new(&key_file).exists());
     let data = std::fs::read_to_string(&key_file).unwrap();
@@ -19,18 +19,18 @@ fn should_generate_random_key() {
 
 #[test]
 fn should_generate_different_keys() {
-    let key_file1 = "should_generate_different_keys1";
+    let key_file1 = PathBuf::from("should_generate_different_keys1");
     let encryptor = crypt::Encryptor::new();
-    encryptor.save_key(key_file1).unwrap();
+    encryptor.save_key(key_file1.as_path()).unwrap();
 
     let data1 = std::fs::read("should_generate_different_keys1").unwrap();
     std::fs::remove_file(key_file1).unwrap();
 
-    let key_file2 = "should_generate_different_keys2";
+    let key_file2 = PathBuf::from("should_generate_different_keys2");
     let encryptor = crypt::Encryptor::new();
-    encryptor.save_key(key_file2).unwrap();
+    encryptor.save_key(key_file2.as_path()).unwrap();
 
-    let data2 = std::fs::read(key_file2).unwrap();
+    let data2 = std::fs::read(key_file2.as_path()).unwrap();
     std::fs::remove_file(key_file2).unwrap();
 
     assert_ne!(data1, data2);
